6cafa605c04a7a37c4a44a8a957b9e148f3811c8
Manuel Lino <mllino1@hotmail.com>
2022-07-05 05:09:11 +0100

    init monster-roladexApp

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// import React, { Component } from "react";
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  render() {
+    // const [count, setCount] = useState("");
+
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
