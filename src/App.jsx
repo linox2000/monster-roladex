de7e100feb554168b9c8bb150f1d546104d7b4d1
Manuel Lino <mllino1@hotmail.com>
2022-07-05 15:54:53 +0100

    finished the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import React, { Component } from "react";
 import { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
     };
   }
   componentDidMount() {
@@ -15,13 +16,11 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
   render() {
-    
-
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters ={this.state.monsters}>
+   
+        </CardList>
       </div>
     );
   }
