2d757fda88124dbd34c6ada50eb3f4aa54fff07a
Manuel Lino <mllino1@hotmail.com>
2022-07-12 08:17:48 +0100

    update the app with component functions using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,71 @@
-// import React, { Component } from "react";
+import React, { useState } from "react";
 import { Component } from "react";
+
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 import "./App.css";
 
-class App extends Component {
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+
+  const [monsters, setMonsters] = useState([]);
+  
+
+  const filterdMonsters = monsters.filter((monster) =>
+  monster.name.toLowerCase().includes(searchField.toLowerCase())
+);
+
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value;
+    setSearchField(searchFieldString)
+  };
+
+  return (
+    <div className="App">
+      <h1>Monster Rolodex</h1>
+      <SearchBox
+        placeholder="search monster"
+        handleChange={onSearchChange}
+      />
+      
+      <CardList monsters={filterdMonsters} /> 
+    </div>
+  );
+};
+
+/*class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
       searchFiled: "",
+      title: "Monster Rolodax",
     };
   }
+  componentDidUpdate(){
+    console.log('componentDidUpdate')
+  }
   componentDidMount() {
+    console.log('componentDidMount')
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
   handleChange = (e) => {
+    this.setState({ searchFiled: e.target.value, title: e.target.value });
+  };
+  onSearchChange = (e) => {
     this.setState({ searchFiled: e.target.value });
   };
   render() {
-    const { monsters, searchFiled } = this.state;
+    const { monsters, searchFiled, title } = this.state;
     const filterdMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchFiled.toLowerCase())
     );
 
     return (
       <div className="App">
-        <h1>Monster Rolodex</h1>
+        <h1>{title}</h1>
         <SearchBox
           placeholder="search monster"
           handleChange={this.handleChange}
@@ -37,6 +74,6 @@ class App extends Component {
       </div>
     );
   }
-}
+}*/
 
 export default App;
